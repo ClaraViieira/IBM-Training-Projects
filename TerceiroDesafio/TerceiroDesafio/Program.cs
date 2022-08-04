4f0cb47ec339ccec106b298fb1137cab719bb88d
ClaraViieira <mariahclara_@hotmail.com>
2022-08-04 15:41:53 -0300

    Terceiro Desafio

---
+++ TerceiroDesafio/TerceiroDesafio/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace TerceiroDesafio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double raio, area, pi = 3.14159;
+
+            raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = pi * raio * raio;
+
+            Console.WriteLine("A = " + area.ToString("F4", CultureInfo.InvariantCulture));
+        }
+    }
+}
