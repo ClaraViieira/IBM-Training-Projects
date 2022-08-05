192bb64d39b7f9d45bdf6f986706d270662a9b1f
ClaraViieira <mariahclara_@hotmail.com>
2022-08-05 15:39:42 -0300

    Quarto Desafio

---
+++ QuartoDesafio/QuartoDesafio/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace QuartoDesafio
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            int func, horasTrabalhadas;
+            double valorHora, salario;
+            func = int.Parse(Console.ReadLine());
+            horasTrabalhadas = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            valorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            salario = horasTrabalhadas * valorHora;
+            Console.WriteLine("NUMBER = " + func);
+            Console.WriteLine("SALARY = U$ " + salario.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
