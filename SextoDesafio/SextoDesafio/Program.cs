760e677051cccf4d9016c446f6a4f0d0a9996f08
ClaraViieira <mariahclara_@hotmail.com>
2022-08-10 15:48:58 -0300

    Sexto Desafio

---
+++ SextoDesafio/SextoDesafio/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SextoDesafio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            ImprimeTabuada(n);
+        }
+        public static void ImprimeTabuada(int n)
+        {
+            for (int i = 1; i <= 10; i++) {
+                int resultado = i * n;
+                Console.WriteLine(i + " x " + n + " = " + resultado);
+            }
+        }
+    }
+}
