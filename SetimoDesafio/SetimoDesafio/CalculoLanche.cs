8ca015f25d0c18185d3edeb23304031cacd8196c
ClaraViieira <mariahclara_@hotmail.com>
2022-08-11 16:11:48 -0300

    Setimo Desafio

---
+++ SetimoDesafio/SetimoDesafio/CalculoLanche.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace SetimoDesafio
+{
+    class CalculoLanche
+    {
+        public static double ValorTotal(int codigo, int quantidade)
+        {
+            double total;
+            if (codigo == 1)
+                total = quantidade * 4.00;
+            else if (codigo == 2)
+                total = quantidade * 4.50;
+            else if (codigo == 3)
+                total = quantidade * 5.00;
+            else if (codigo == 4)
+                total = quantidade * 2.00;
+            else
+                total = quantidade * 1.50;
+            return total;
+        }
+    }
+}
---
+++ SetimoDesafio/SetimoDesafio/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace SetimoDesafio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vet = Console.ReadLine().Split(' ');
+            int codigo = int.Parse(vet[0]);
+            int quantidade = int.Parse(vet[1]);
+            double somaTotal = CalculoLanche.ValorTotal(codigo, quantidade);
+            Console.WriteLine("Total: R$ " + somaTotal.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
