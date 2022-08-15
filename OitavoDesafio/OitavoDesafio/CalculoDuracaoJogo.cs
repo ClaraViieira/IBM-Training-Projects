073775d6ec866a636824c4bc4faf5f57dadc0f41
ClaraViieira <mariahclara_@hotmail.com>
2022-08-15 09:29:36 -0300

    Oitavo Desafio

---
+++ OitavoDesafio/OitavoDesafio/CalculoDuracaoJogo.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OitavoDesafio
+{
+    class CalculoDuracaoJogo
+    {
+        public static int CalculoHoras(int horaInicial, int minutoInicial, int horaFinal, int minutoFinal)
+        {
+            int instanteInicial = horaInicial * 60 + minutoInicial;
+            int instanteFinal = horaFinal * 60 + minutoFinal;
+
+            int duracao;
+            if (instanteInicial < instanteFinal)
+                duracao = instanteFinal - instanteInicial;
+            else 
+                duracao = (24 * 60 - instanteInicial) + instanteFinal;
+
+            int duracaoHoras = duracao / 60;
+            int duracaoMinutos = duracao % 60;
+
+            return duracaoHoras;
+        }
+        public static int CalculoMinutos(int horaInicial, int minutoInicial, int horaFinal, int minutoFinal)
+        {
+            int instanteInicial = horaInicial * 60 + minutoInicial;
+            int instanteFinal = horaFinal * 60 + minutoFinal;
+
+            int duracao;
+            if (instanteInicial < instanteFinal)
+                duracao = instanteFinal - instanteInicial;
+            else
+                duracao = (24 * 60 - instanteInicial) + instanteFinal;
+
+            int duracaoHoras = duracao / 60;
+            int duracaoMinutos = duracao % 60;
+
+            return duracaoMinutos;
+        }
+    }
+}
---
+++ OitavoDesafio/OitavoDesafio/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using System;
+
+namespace OitavoDesafio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] valores = Console.ReadLine().Split(' ');
+            int horaInicial = int.Parse(valores[0]);
+            int minutoInicial = int.Parse(valores[1]);
+            int horaFinal = int.Parse(valores[2]);
+            int minutoFinal = int.Parse(valores[3]);
+            int valorHoras = CalculoDuracaoJogo.CalculoHoras(horaInicial, minutoInicial, horaFinal, minutoFinal);
+            int valorMinutos = CalculoDuracaoJogo.CalculoMinutos(horaInicial, minutoInicial, horaFinal, minutoFinal);
+            Console.WriteLine("O JOGO DUROU " + valorHoras + " HORA(S) E " + valorMinutos + " MINUTO(S)");
+
+        }
+    }
+}
