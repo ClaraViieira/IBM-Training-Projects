62b0a4aa386d5e47a9dd54c33ba85e2f9a738708
ClaraViieira <mariahclara_@hotmail.com>
2022-08-17 11:01:28 -0300

    Nono Desafio

---
+++ NonoDesafio/NonoDesafio/CalculoSalario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace NonoDesafio {
+    class CalculoSalario {
+        public double Salario { get; set; }
+        public double Reajuste { get; set; }
+        public double Percentual { get; set; }
+        public double ValorReajustado { get; set; }
+        public double NovoSalario { get; set; }
+        public CalculoSalario(double salario) {
+            Salario = salario;
+            if (salario <= 400.00)
+                Percentual = 15.0;
+            else if (salario <= 800.00)
+                Percentual = 12.0;
+            else if (salario <= 1200.00)
+                Percentual = 10.0;
+            else if (salario <= 2000.00)
+                Percentual = 7.0;
+            else
+                Percentual = 4.0;
+
+            ValorReajustado = Salario * Percentual / 100.00;
+
+            NovoSalario = Salario + Salario * Percentual / 100.00;
+        }
+    }
+}
---
+++ NonoDesafio/NonoDesafio/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+
+namespace NonoDesafio {
+    class Program {
+        static void Main(string[] args) {
+            double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            CalculoSalario calculo = new CalculoSalario(salario);
+            Console.WriteLine("Novo salário: " + calculo.NovoSalario.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Reajuste ganho: " + calculo.ValorReajustado.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Em percentual: " + calculo.Percentual + "%");
+        }
+    }
+}
