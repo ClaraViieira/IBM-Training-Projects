68f1717711a59e3a9a5c46ca62c0219aa2dd9f6a
ClaraViieira <mariahclara_@hotmail.com>
2022-08-17 13:14:09 -0300

    Décimo Desafio

---
+++ DecimoDesafio/DecimoDesafio/ClassificacaoAnimal.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DecimoDesafio {
+    class ClassificacaoAnimal {
+        public ClassificacaoAnimal(string primeiroAnimal, string segundoAnimal, string terceiroAnimal) {
+            if (primeiroAnimal == "vertebrado") {
+                if (segundoAnimal == "ave") {
+                    if (terceiroAnimal == "carnivoro") {
+                        Console.WriteLine("aguia");
+                    }
+                    else {
+                        Console.WriteLine("pomba");
+                    }
+                }
+                else {
+                    if (terceiroAnimal == "onivoro") {
+                        Console.WriteLine("homem");
+                    }
+                    else {
+                        Console.WriteLine("vaca");
+                    }
+                }
+            }
+            else {
+                if (segundoAnimal == "inseto") {
+                    if (terceiroAnimal == "hematofago") {
+                        Console.WriteLine("pulga");
+                    }
+                    else {
+                        Console.WriteLine("lagarta");
+                    }
+                }
+                else {
+                    if (terceiroAnimal == "hematofago") {
+                        Console.WriteLine("sanguessuga");
+                    }
+                    else {
+                        Console.WriteLine("minhoca");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ DecimoDesafio/DecimoDesafio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DecimoDesafio {
+    class Program {
+        static void Main(string[] args) {
+            string primeiroAnimal = Console.ReadLine();
+            string segundoAnimal = Console.ReadLine();
+            string terceiroAnimal = Console.ReadLine();
+            ClassificacaoAnimal nomeAnimal = new ClassificacaoAnimal(primeiroAnimal, segundoAnimal, terceiroAnimal);
+        }
+    }
+}
