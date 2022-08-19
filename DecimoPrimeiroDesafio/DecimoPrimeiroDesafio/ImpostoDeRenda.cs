b56df03626741db7000e427225c125aaaa711c46
ClaraViieira <mariahclara_@hotmail.com>
2022-08-19 11:31:41 -0300

    Décimo Primeiro Desafio

--- DecimoPrimeiroDesafio/DecimoPrimeiroDesafio/ImpostoDeRenda.cs
+++ DecimoPrimeiroDesafio/DecimoPrimeiroDesafio/ImpostoDeRenda.cs
@@ -5,26 +5,24 @@ using System.Globalization;
 
 namespace DecimoPrimeiroDesafio {
     class ImpostoDeRenda {
-        public double Salario { get; set; }
+        public double Salario { get; private set; }
         public ImpostoDeRenda(double salario) {
             Salario = salario;
         }
-        public double Imposto() {
-            double[] primeiraFaixa = new double[] { 0, 2000 };
-            double[] segundaFaixa = new double[] { 2000.01, 3000 };
-            double[] terceiraFaixa = new double[] { 3000.01, 4500 };
-            if (Salario > primeiraFaixa[0] && Salario <= primeiraFaixa[1])
+        private double Imposto() {
+            double[] Faixa = new double[] { 0, 2000, 2000.01, 3000, 3000.01, 4500 };
+            if (Salario > Faixa[0] && Salario <= Faixa[1])
                 return 0;
-            else if (Salario >= segundaFaixa[0] && Salario <= segundaFaixa[1])
+            else if (Salario >= Faixa[2] && Salario <= Faixa[3])
                 return (Salario - 2000) * 0.08;
-            else if (Salario >= terceiraFaixa[0] && Salario <= terceiraFaixa[1])
+            else if (Salario >= Faixa[4] && Salario <= Faixa[5])
                 return (Salario - 3000) * 0.18 + 1000 * 0.08;
             else
                 return (Salario - 4500) * 0.28 + 1500 * 0.18 + 1000 * 0.08;
         }
         public override string ToString() {
         double valor = Imposto();
-            return valor == 0 ? "Isento" : "R$ " + valor.ToString("F2", CultureInfo.InvariantCulture);
+            return valor == 0 ? "Isento" : "Imposto igual a R$ " + valor.ToString("F2", CultureInfo.InvariantCulture);
         }
     }
 }
--- DecimoPrimeiroDesafio/DecimoPrimeiroDesafio/Program.cs
+++ DecimoPrimeiroDesafio/DecimoPrimeiroDesafio/Program.cs
@@ -4,7 +4,7 @@ using System.Globalization;
 namespace DecimoPrimeiroDesafio {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Informe seu salário: ");
+            Console.Write("Informe seu salário: ");
             double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             ImpostoDeRenda imposto = new ImpostoDeRenda(salario);
             Console.WriteLine(imposto);
