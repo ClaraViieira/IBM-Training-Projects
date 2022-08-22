7daa673de7f53501e19e40669443c6c54e50d2fc
ClaraViieira <mariahclara_@hotmail.com>
2022-08-22 12:48:53 -0300

    Décimo Terceiro Desafio

--- DecimoTerceiroDesafio/DecimoTerceiroDesafio/Program.cs
+++ DecimoTerceiroDesafio/DecimoTerceiroDesafio/Program.cs
@@ -4,7 +4,7 @@ namespace DecimoTerceiroDesafio {
     class Program {
         static void Main(string[] args) {
             Console.Write("Insira um valor inteiro: ");
-            int valor = int.Parse(Console.ReadLine());
+            var valor = int.Parse(Console.ReadLine());
             ConversaoTempo tempo = new ConversaoTempo(valor);
             Console.WriteLine(tempo.Conversao());
         }
