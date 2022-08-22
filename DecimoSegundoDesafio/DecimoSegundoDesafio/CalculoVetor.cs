66a10412a980ea3048db34b77c4f15f132226630
ClaraViieira <mariahclara_@hotmail.com>
2022-08-22 11:21:10 -0300

    Décimo Segundo Desafio

---
+++ DecimoSegundoDesafio/DecimoSegundoDesafio/CalculoVetor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DecimoSegundoDesafio {
+    static class CalculoVetor {
+        public static string Menor_E_Posicao(string[] vetor) {
+            int menor = int.Parse(vetor[0]);
+            int posicao = 0;
+            for (int i = 0; i < vetor.Length; i++) {
+                if (int.Parse(vetor[i]) < menor) {
+                    menor = Convert.ToInt32(vetor[i]);
+                    posicao = i;
+                }
+            }
+            return menor.ToString() + "," + posicao.ToString();
+        }
+    }
+}
---
+++ DecimoSegundoDesafio/DecimoSegundoDesafio/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DecimoSegundoDesafio {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Insira o tamanho do vetor: ");
+            int tamanho = int.Parse(Console.ReadLine());
+            Console.WriteLine("Insira os valores necessários abaixo: ");
+            string[] vetor = Console.ReadLine().Split(' ');
+            string[] menorPosicao = CalculoVetor.Menor_E_Posicao(vetor).Split(',');
+            Console.WriteLine("O menor número é " + menorPosicao[0] + " na posição " + menorPosicao[1]);
+        }
+    }
+}
