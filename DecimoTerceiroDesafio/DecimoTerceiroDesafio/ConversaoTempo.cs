a772ca6e9a87e420dbe6ea6d62cfc60c0d013874
ClaraViieira <mariahclara_@hotmail.com>
2022-08-22 12:45:08 -0300

    Décimo Terceiro Desafio

---
+++ DecimoTerceiroDesafio/DecimoTerceiroDesafio/ConversaoTempo.cs
@@ -0,0 +1,20 @@
+﻿namespace DecimoTerceiroDesafio {
+    class ConversaoTempo {
+        private int Horas { get; set; }
+        private int Minutos { get; set; }
+        private int Segundos { get; set; }
+        private int Resto { get; set; }
+        private int Valor { get; set; }
+        public ConversaoTempo(int valor) {
+            Valor = valor;
+        }
+        public string Conversao() {
+            Horas = Valor / 3600;
+            Resto = Valor % 3600;
+            Minutos = Resto / 60;
+            Resto = Resto % 60;
+            Segundos = Resto;
+            return Horas + ":" + Minutos + ":" + Segundos;
+        }
+    }
+}
---
+++ DecimoTerceiroDesafio/DecimoTerceiroDesafio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DecimoTerceiroDesafio {
+    class Program {
+        static void Main(string[] args) {
+            Console.Write("Insira um valor inteiro: ");
+            int valor = int.Parse(Console.ReadLine());
+            ConversaoTempo tempo = new ConversaoTempo(valor);
+            Console.WriteLine(tempo.Conversao());
+        }
+    }
+}
