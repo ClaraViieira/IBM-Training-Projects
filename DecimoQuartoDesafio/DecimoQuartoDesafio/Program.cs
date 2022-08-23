27a2eb47af6e95071672088bc2d77b5653d54d92
ClaraViieira <mariahclara_@hotmail.com>
2022-08-23 16:01:58 -0300

    Décimo Quarto Desafio

---
+++ DecimoQuartoDesafio/DecimoQuartoDesafio/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DecimoQuartoDesafio {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Entre com dois valores na mesma linha: ");
+            string[] valores = Console.ReadLine().Split(' ');
+            int x = int.Parse(valores[0]);
+            int y = int.Parse(valores[1]);
+            SistemaCartesiano.PlanoCartesiano(valores, x, y);
+        }
+    }
+}
---
+++ DecimoQuartoDesafio/DecimoQuartoDesafio/SistemaCartesiano.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DecimoQuartoDesafio {
+    class SistemaCartesiano {
+        public static void PlanoCartesiano(string[] valores, int x, int y) {
+            if (x == 0 || y == 0)
+                Console.WriteLine("Nulo");
+            else if (x > 0 && y > 0)
+                Console.WriteLine("Primeiro quadrante");
+            else if (x < 0 && y > 0)
+                Console.WriteLine("Segundo quadrante");
+            else if (x < 0 && y < 0)
+                Console.WriteLine("Terceiro quadrante");
+            else if (x > 0 && y < 0)
+                Console.WriteLine("Quarto quadrante");
+        }
+    }
+}
