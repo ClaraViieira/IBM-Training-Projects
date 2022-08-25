790e8102d59d6bcec4a2fbb9534ff9096b2cdfd3
ClaraViieira <mariahclara_@hotmail.com>
2022-08-25 10:27:38 -0300

    Décimo quinto desafio

---
+++ DecimoQuintoDesafio/DecimoQuintoDesafio/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DecimoQuintoDesafio {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Insira dois valores inteiros: ");
+            int x = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            SomaImpares soma = new SomaImpares(x, y);
+            Console.WriteLine(soma);
+        }
+    }
+}
---
+++ DecimoQuintoDesafio/DecimoQuintoDesafio/SomaImpares.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DecimoQuintoDesafio {
+    class SomaImpares {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public SomaImpares(int x, int y) {
+            X = x;
+            Y = y;
+        }
+        public int Soma() {
+            int min, max;
+            if (X < Y) {
+                min = X;
+                max = Y;
+            }
+            else {
+                min = Y;
+                max = X;
+            }
+            int soma = 0;
+            for (int i = min + 1; i < max; i++) {
+                if (i % 2 != 0) {
+                    soma = soma + i;
+                }
+            } 
+            return soma;
+        }
+        public override string ToString() {
+            int resultado = Soma();
+            return "Soma dos ímpares consecutivos: " + resultado.ToString();
+        }
+    }
+}
