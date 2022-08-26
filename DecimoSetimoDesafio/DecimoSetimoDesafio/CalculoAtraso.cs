4cf58ea18259b4d6b5aeb85929dc89f706fba08f
ClaraViieira <mariahclara_@hotmail.com>
2022-08-26 14:49:32 -0300

    Décimo Sétimo Desafio

---
+++ DecimoSetimoDesafio/DecimoSetimoDesafio/CalculoAtraso.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DecimoSetimoDesafio {
+    class CalculoAtraso {
+        public int Hora { get; private set; }
+        public int Minuto { get; private set; }
+        public CalculoAtraso(int hora, int minuto) {
+            Hora = hora;
+            Minuto = minuto;
+        }
+        private int Atraso() {
+            if (Hora == 7)
+               return Minuto;
+            else if (Hora == 8)
+               return 60 + Minuto;
+            else if (Hora == 9)
+               return 120 + Minuto;
+            else
+               return 0;
+        }
+        public override string ToString() {
+            int resultado = Atraso();
+            return "Atraso maximo em minutos: " + resultado;
+        }
+    }
+}
---
+++ DecimoSetimoDesafio/DecimoSetimoDesafio/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DecimoSetimoDesafio {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Insira o horário que Bino acordou: ");
+            string[] horas = Console.ReadLine().Split(':');
+            int hora = int.Parse(horas[0]);
+            int minuto = int.Parse(horas[1]);
+            CalculoAtraso atraso = new CalculoAtraso(hora, minuto);
+            Console.WriteLine(atraso);
+        }
+    }
+}
