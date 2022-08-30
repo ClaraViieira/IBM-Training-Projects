b4cfcfa31f523b352c39f8971efdefa29277e9a8
ClaraViieira <mariahclara_@hotmail.com>
2022-08-30 09:41:09 -0300

    Décimo oitavo desafio

---
+++ DecimoOitavoDesafio/DecimoOitavoDesafio/Jogo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DecimoOitavoDesafio {
+    class Jogo {
+        private string PrimeiroJogador { get; set; }
+        private string SegundoJogador { get; set; }
+
+        public Jogo(string primeiroJogador, string segundoJogador) {
+            PrimeiroJogador = primeiroJogador;
+            SegundoJogador = segundoJogador;
+        }
+
+        public string Brincadeira() {
+            if (PrimeiroJogador == "ataque" && SegundoJogador == "ataque")
+                return "Aniquilação mútua!";
+            if (PrimeiroJogador == "papel" && SegundoJogador == "papel")
+                return "Ambos venceram!";
+            if (PrimeiroJogador == "ataque")
+                return "Jogador 1 venceu!";
+            if (SegundoJogador == "ataque")
+                return "Jogador 2 venceu!";
+            if (SegundoJogador == "papel")
+                return "Jogador 1 venceu!";
+            if (PrimeiroJogador == "papel")
+                return "Jogador 2 venceu!";
+            else
+                return "Sem ganhador!";
+        }
+        public override string ToString() {
+            string vencedor = Brincadeira();
+            return vencedor;
+        }
+    }
+}
---
+++ DecimoOitavoDesafio/DecimoOitavoDesafio/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DecimoOitavoDesafio {
+    class Program {
+        static void Main(string[] args) {
+            try {
+                Console.Write("Insira a quantidade de rodadas: ");
+                int n = int.Parse(Console.ReadLine());
+                for (int i = 1; i <= n; i++) {
+                    Console.Write("Primeiro jogador: ");
+                    string primeiroJogador = Console.ReadLine();
+                    Console.Write("Segundo jogador: ");
+                    string segundoJogador = Console.ReadLine();
+                    Jogo jogo = new Jogo(primeiroJogador, segundoJogador);
+                    Console.WriteLine(jogo);
+                }
+            }
+            catch (FormatException) {
+                Console.WriteLine("Erro, digite um número inteiro!");
+                    }
+        }
+    }
+}
