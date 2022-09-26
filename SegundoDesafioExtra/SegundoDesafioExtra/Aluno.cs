707ca775dcd54babe5e64c776ac9e7280652dbb4
ClaraViieira <mariahclara_@hotmail.com>
2022-09-26 10:57:42 -0300

    Segundo desafio extra

---
+++ SegundoDesafioExtra/SegundoDesafioExtra/Aluno.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SegundoDesafioExtra
+{
+    class Aluno
+    {
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+        public int Idade { get; set; }
+
+        public Aluno(string nome, string endereco, int idade)
+        {
+            Nome = nome;
+            Endereco = endereco;
+            Idade = idade;
+        }
+
+        public void ImprimirDados()
+        {
+            Console.WriteLine("Nome: " + Nome + "\n" 
+                + "Endereço: " + Endereco + "\n" 
+                + "Idade: " + Idade);
+        }
+
+        public void ImprimirTodasMaterias()
+        {
+            Console.WriteLine("Insira as notas de matemática, português e geografia respectivamente: ");
+            string[] notas = Console.ReadLine().Split(' ');
+            decimal matematica = decimal.Parse(notas[0], CultureInfo.InvariantCulture);
+            decimal portugues = decimal.Parse(notas[1], CultureInfo.InvariantCulture);
+            decimal geografia = decimal.Parse(notas[2], CultureInfo.InvariantCulture);
+            ImprimirDados();
+            Console.WriteLine("Nota de matemática: " + matematica.ToString("F2", CultureInfo.InvariantCulture) + "\n"
+                + "Nota de português: " + portugues.ToString("F2", CultureInfo.InvariantCulture) + "\n"
+                + "Nota de geografia: " + geografia.ToString("F2", CultureInfo.InvariantCulture) + "\n");
+        }
+        public void ImprimirNotaMatematica()
+        {
+            Console.WriteLine("Insira a nota de matemática: ");
+            decimal matematica = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            ImprimirDados();
+            Console.WriteLine("Nota de matemática: " + matematica.ToString("F2", CultureInfo.InvariantCulture));
+        }
+        public void ImprimirNotaPortugues()
+        {
+            Console.WriteLine("Insira a nota de português: ");
+            decimal portugues = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            ImprimirDados();
+            Console.WriteLine("Nota de português: " + portugues.ToString("F2", CultureInfo.InvariantCulture));
+        }
+        public void ImprimirNotaGeografia()
+        {
+            Console.WriteLine("Insira a nota de geografia: ");
+            decimal geografia = decimal.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            ImprimirDados();
+            Console.WriteLine("Nota de geografia: " + geografia.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
---
+++ SegundoDesafioExtra/SegundoDesafioExtra/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace SegundoDesafioExtra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bem vindo ao sistema de notas do alunos!" + "\n" + "Digite o nome do aluno: ");
+            string nome = Console.ReadLine();
+            Console.Write("Digite o endereço do aluno: ");
+            string endereco = Console.ReadLine();
+            Console.Write("Digite a idade do aluno: ");
+            int idade = int.Parse(Console.ReadLine());
+
+            Aluno aluno = new Aluno(nome, endereco, idade);
+
+            string opcao = string.Empty;
+            bool menu = true; 
+
+            while (menu)
+            {
+                Console.Clear();
+                Console.WriteLine("Escolha uma das opções abaixo: ");
+                Console.WriteLine("1 - Todas as matérias: ");
+                Console.WriteLine("2 - Nota de matemática: ");
+                Console.WriteLine("3 - Nota de português: ");
+                Console.WriteLine("4 - Nota de geografia: ");
+                Console.WriteLine("5 - Encerrar ");
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        aluno.ImprimirTodasMaterias();
+                        break;
+                    case "2":
+                        aluno.ImprimirNotaMatematica();
+                        break;
+                    case "3":
+                        aluno.ImprimirNotaPortugues();
+                        break;
+                    case "4":
+                        aluno.ImprimirNotaGeografia();
+                        break;
+                    case "5":
+                        menu = false;
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida");
+                        break;
+                }
+                Console.WriteLine("Pressione qualquer tecla para continuar...");
+                Console.ReadLine();
+            }
+            Console.WriteLine("Programa encerrado com sucesso!");
+        }
+    }
+}
